cd0583fbe1051e0cad42dab54cfe42c5f256ff7d
SNake <snake_goodluck@hotmail.com>
2018-02-09 22:02:47 +0800

    兼容ttd和业务流测试

--- src/test/case/test_search.py
+++ src/test/case/test_search.py
@@ -2,21 +2,22 @@
 __author__ = 'snake'
 
 import unittest
-from ddt import data,ddt,file_data,unpack
+
 from config import config
 from src.test.common import common
 from src.utils.util_xml import XmlUtils
+from ddt import data,ddt,file_data,unpack
 from src.test.page.page_index import IndexPage
 
 
 @ddt
-class TestCaseLogin(unittest.TestCase):
+class TestCaseSearch(unittest.TestCase):
+    global keys
+    keys = XmlUtils.read_xml_document(config.DATA_FILE_PATH, "test_search_success")
 
     # 测试方法执行前执行
     def setUp(self):
-        global keys
         self.browser = common.get_browser()
-        keys = XmlUtils.read_xml_document(config.DATA_FILE_PATH, "test_search_fail")
 
 
     # 测试方法执行后执行
@@ -24,12 +25,11 @@ class TestCaseLogin(unittest.TestCase):
         self.browser.close()
 
 
+    # 测试搜索成功
     @data(*keys)
     def test_search_success(self, key):
-        self.browser = common.get_browser()
         result_page = IndexPage(browser = self.browser).search(key.get("ky"))
         assert result_page.toindex.text == "百度首页"
-        self.browser.close()
 
     def test_click_news(self):
         IndexPage(browser = self.browser).news_link()
